83043b2a641acfe8b8bbd9845235dcdc41dc1364
Clyde <jcayap18@gmail.com>
2024-04-01 21:12:15 +0800

    revert back backend url

--- src/backend_url.js
+++ src/backend_url.js
@@ -1,3 +1,2 @@
 // export const BASE_URL = 'http://127.0.0.1:8000';
-// export const BASE_URL = 'https://ny-tracking-system-backend.onrender.com';
-export const BASE_URL = 'http://18.224.57.76:8000';
+export const BASE_URL = 'https://ny-tracking-system-backend.onrender.com';
